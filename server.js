159d341a4f128ece600d9367e23b1fed615ab491
Chris Corey <chriscorey.dev@gmail.com>
2018-09-13 00:14:36 -0600

    Adding mysql

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const path = require("path");
+const mysql = require("mysql");
 const app = express();
+const dbInfo = require("./dbInfo.json");
 
 app.use(express.static(path.join(__dirname, "build")));
+
+const SELECT_ALL_FILMS_QUERTY = "SELECT * FROM film";
+
+const conn = mysql.createConnection(dbInfo);
+conn.connect(err => {
+  if (err) return console.log(err);
+  console.log("connected as id " + conn.threadId);
+  console.log(conn.state);
+});
 
 const users = [
   {
@@ -32,17 +43,29 @@ app.get("/api/users/:id", (req, res) => {
   res.send(user);
 });
 
+// API get request before serving React
+app.get("/api/sakila/films", (req, res) => {
+  conn.query(SELECT_ALL_FILMS_QUERTY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else return res.send(results);
+    // res.json({
+    //   data: results
+    // });
+  });
+});
+
 // Serving React
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
-// Port stuffs
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
-});
+// // Port stuffs
+// const port = process.env.PORT || 3002;
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}...`);
+// });
 
-function notFound(res) {
-  res.status(404).send({ detail: "Not Found" });
-}
+// function notFound(res) {
+//   res.status(404).send({ detail: "Not Found" });
+// }
