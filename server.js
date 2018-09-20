23ffcca96fed06dd1e764245eeb04b1a9d16a473
Chris Corey <chriscorey.dev@gmail.com>
2018-09-19 23:37:00 -0600

    Added Database class, refactored code to use it

--- server.js
+++ server.js
@@ -9,28 +9,50 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "build")));
 
-const conn = mysql.createConnection(dbInfo);
+// const conn = mysql.createConnection(dbInfo);
 
 // Prioritized goals
 // TODO: Make into NPM package
 // TODO: CRUD stuff
+// TODO: Currently getting first primary key and only filtering by that.
+//       In the future I'd like to use referenced schemas to automatically do everything.
 // TODO: Authentication/ authorization on CRUD operations
 // TODO: Be able to make more database connections based on what's in settings.json
-// TODO: Error handling. 404, 400.
+// TODO: Error handling. 400, 500?.
 // TODO: Don't wait for databases to query just to tell the app to go to reat GUI
 // TODO: React's caching is poo in Chrome
 
-// TODO: Make query parameter to count
-// TODO: Make query parameter to sort (alphabetically? idk)
-// TODO: Make query parameter to limit amount of results
-// TODO: ? Make runQuery function and make it asyncronous so it has to wait for it to stop
+// TODO: Query parameters to count
+//       sort (alphabetically? idk)
+//       limit amount of results
 // TODO: Configure server's git with www-data to give that user ownership
+
+class Database {
+  constructor(config) {
+    this.connection = mysql.createConnection(config);
+  }
+  query(sql, args) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(sql, args, (err, rows) => {
+        if (err) return reject(err);
+        resolve(rows);
+      });
+    });
+  }
+  close() {
+    return new Promise((resolve, reject) => {
+      this.connection.end(err => {
+        if (err) return reject(err);
+        resolve();
+      });
+    });
+  }
+}
 
 (async () => {
   // Need to wait for API before deciding to make function or not
 
   // /api/  -  Lists dbs
-  // app.get(settings.path, (req, res) =>
   app.get("/api", (req, res) =>
     res.send(
       settings.server.databases.map(database => {
@@ -39,105 +61,118 @@ const conn = mysql.createConnection(dbInfo);
       })
     )
   );
+
   await createAPI();
 
   // Serving React
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
   });
-  // Port stuffs
+
+  // Handling port
   const port = process.env.PORT || settings.server.port;
   app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
   });
 })();
 
-// This does a lot of the work
+// Creating the API
 async function createAPI() {
-  conn.connect(err => {
-    if (err) return err;
+  settings.server.databases.forEach(db => {
+    const mysqlInfo = require(db.mysqlInfo);
+    const database = new Database(mysqlInfo);
 
-    conn.query("SHOW FULL TABLES WHERE Table_Type != 'VIEW'", (err, tables) => {
-      if (err) return err;
-
-      // /api/{db}  -  Lists tables in db
-      app.get(settings.server.path, (req, res) =>
-        res.send(tables.map(table => table[`Tables_in_${dbInfo.database}`]))
-      );
-
-      // /api/sakila/{table} & /api/{db}/{table}/:id
-      tables.forEach(table => {
-        conn.query(
-          `SHOW COLUMNS IN ${table[`Tables_in_${dbInfo.database}`]}`,
-          (err, rows) => {
-            if (err) return err;
-            rows.find(
-              row =>
-                row.Extra === "auto_increment"
-                  ? addURL(table[`Tables_in_${dbInfo.database}`], row.Field)
-                  : addURL(table[`Tables_in_${dbInfo.database}`]) // No primary key, so no ID field
-            );
-          }
+    database
+      .query("SHOW FULL TABLES WHERE Table_Type != 'VIEW'")
+      .then(tables => {
+        // /api/{db}  -  Lists tables in db
+        app.get(`${path}/${mysqlInfo.database}`, (req, res) =>
+          res.send(
+            tables.map(table => table[`Tables_in_${mysqlInfo.database}`])
+          )
         );
+
+        // /api/{db}/{table} & /api/{db}/{table}/:id
+        tables.forEach(table => {
+          const tableName = table[`Tables_in_${mysqlInfo.database}`];
+
+          database.query(`SHOW COLUMNS IN ${tableName}`).then(cols => {
+            let pkField = [];
+            cols.find(col => {
+              col.Key === "PRI" && pkField.push(col.Field);
+            });
+            addURL(tableName, pkField[0], database, mysqlInfo);
+          });
+        });
       });
-    });
   });
   // Can this be done better?
   return await new Promise((resolve, reject) => setTimeout(resolve, 500));
 }
 
-function addURL(table, idField) {
-  app.get(`${settings.server.path}/${table}`, (req, res) => {
-    console.log(`SELECT * FROM ${table}`);
-    conn.query(
-      `SELECT * FROM ${table}`,
-      (err, rows) => (err ? res.send(err) : res.send(rows))
-    );
-  });
+function addURL(tableName, pkField, database, mysqlInfo) {
+  // GET
+  app.get(
+    `${settings.server.path}/${mysqlInfo.database}/${tableName}`,
+    (req, res) => {
+      database.query(`SELECT * FROM ${tableName}`).then(rows => res.send(rows));
+    }
+  );
 
-  app.get(`${settings.server.path}/${table}/:id`, (req, res) => {
-    // app.get(`${settings.server.path}/:table/:id`, (req, res) => {    // TODO: LOOK INTO THIS
+  app.get(
+    `${settings.server.path}/${mysqlInfo.database}/${tableName}/:id`,
+    (req, res) => {
+      database
+        .query(
+          `SELECT * FROM ${tableName} WHERE ${pkField} = '${req.params.id}'`
+        )
+        .then(rows => {
+          if (rows.length === 0) return NotFound;
+          return rows[0];
+        })
+        .then(output => res.send(output));
+    }
+  );
 
-    // If an ID Field was identified, it will GET that. Else it'll GET what's at the row index
-    const query = idField
-      ? `SELECT * FROM ${table} WHERE ${idField} = ${req.params.id}`
-      : `SELECT * FROM ${table} LIMIT 1 OFFSET ${parseInt(req.params.id) - 1}`;
-    conn.query(query, (err, rows) => (err ? res.send(err) : res.send(rows[0])));
-  });
+  // PUT
+  app.put(
+    `${settings.server.path}/${mysqlInfo.database}/${tableName}/:id`,
+    (req, res) => {
+      const props = Object.keys(req.body);
 
-  app.put(`${settings.server.path}/${table}/:id`, (req, res) => {
-    // TODO: Use Joi for validation
-    // if (!req.body) return res.status(400).send({ error: "badd sauce" });
-    // if (!prop)
-    //   return res.status(400).send({ error: `No field with name of ${prop}` });
+      // TODO: Error handling in a bundle. This needs to attempt all at once and not run any commands if it fails for any reason.
+      // Runs through each item to change in the
+      // TODO: Errors:
+      //       Can't change Primary Key's field
+      //       Can't be null when !NULL.
 
-    const props = Object.keys(req.body);
-    props.forEach(prop => {
-      conn.query(
-        `UPDATE ${table} SET ${prop} = '${req.body[prop]}' WHERE ${idField} = ${
-          req.params.id
-        }`,
-        (err, rows) => {
-          if (err) return res.send(err);
+      // TODO: Use Joi for validation
+      // // Checking if body is valid:
 
-          // Return GET Request of modified data
-          const query = idField
-            ? `SELECT * FROM ${table} WHERE ${idField} = ${req.params.id}`
-            : `SELECT * FROM ${table} LIMIT 1 OFFSET ${parseInt(req.params.id) -
-                1}`;
-          conn.query(
-            query,
-            (err, rows) => (err ? res.send(err) : res.send(rows[0]))
+      props.forEach(prop => {
+        database
+          .query(
+            `UPDATE ${tableName} SET ${prop} = '${
+              req.body[prop]
+            }' WHERE ${pkField} = '${req.params.id}'`
+          )
+          .then(
+            database
+              .query(
+                `SELECT * FROM ${tableName} WHERE ${pkField} = '${
+                  req.params.id
+                }'`
+              )
+              .then(rows => res.send(rows[0]))
           );
-        }
-      );
-    });
-  });
+      });
+    }
+  );
 }
 
 // 404
-// {
-//   "statusCode": 404,
-//   "error": "Not Found",
-//   "message": "No endpoint found that matches '/v2/beers//1'"
-//   }
+const NotFound = {
+  statusCode: 404,
+  error: "Not Found",
+  message: "Found no item that matches the ID"
+};
